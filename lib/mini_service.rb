00c41e518062b3924f9030ee6d39872b51934163
vaporyhumo <roanvilina@gmail.com>
2024-04-08 17:01:48 -0400

    Remove all CLI related files

--- lib/mini_service.rb
+++ lib/mini_service.rb
@@ -3,13 +3,8 @@
 
 $LOAD_PATH.push File.expand_path('lib', __dir__)
 
-require 'thor'
-require 'tty-file'
-require 'tty-prompt'
-
 require 'mini_service/base'
 require 'mini_service/error'
-require 'mini_service/file_generator'
 require 'mini_service/version'
 
 module MiniService
